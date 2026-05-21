f3b9f898225685072102e9c0f9c3d9a0fe59a153
Ashish Sharma <ashishhhsharma@gmail.com>
2026-05-21 10:13:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def index():
         </head>
         <body>
             <div class="card">
-                <h1>✨ Hello Dear!</h1>
+                <h1>✨ Hello Deam team!</h1>
                 <p>Your Flask application is running on <b>Google Cloud Run</b>.</p>
                 <p>Deployed via Cloud Build without a Dockerfile.</p>
             </div>
